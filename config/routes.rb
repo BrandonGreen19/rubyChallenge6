bfe68e9f79c5825e1d5ccce3349be7f247ebc2d8
BrandonGreen19 <BrandonGreen1982@gmail.com>
2018-03-16 20:19:19 -0500

    part 2&3 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'customers/index', to: 'customers#index'
+
+  get 'customers/alphebetized', to: 'customers#alphebetized'
+
+  get 'customers/missing_email', to: 'customers#missing_email'
+
+  root to: 'customers#index'
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
